f79d59833317623dd06e05e3b3b62334dd32cd2a
ZZy979 <979481894@qq.com>
2024-10-02 16:28:43 +0800

    添加CalculatorTest

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void evaluatesExpression() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluate("1+2+3");
+        assertEquals(6, sum);
+    }
+}
